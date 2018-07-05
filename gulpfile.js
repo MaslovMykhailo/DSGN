d2c416709dd5243a7517209d92357378cb396463
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-07-05 18:53:55 +0300

    Create studio section

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const browserSync = require('browser-sync');
 const del = require('del');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
 
 gulp.task('less', () => {
   return gulp.src('./frontend/less/**/*.less')
@@ -14,6 +15,13 @@ gulp.task('less', () => {
     .pipe(concat('index.css'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./public'));
+});
+
+gulp.task('js', () => {
+  return gulp.src('./frontend/js/**/*.js')
+    .pipe(concat('index.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public'))
 });
 
 gulp.task('assets', () => {
@@ -26,11 +34,12 @@ gulp.task('clean', () => {
 });
 
 gulp.task('build', gulp.series(
-  'clean', gulp.parallel('less', 'assets'))
+  'clean', gulp.parallel('less', 'assets', 'js'))
 );
 
 gulp.task('watch', () => {
   gulp.watch('./frontend/less/**/*.less', gulp.series('less'));
+  gulp.watch('./frontend/js/**/*.js', gulp.series('js'));
   gulp.watch('./frontend/assets/**/*.*', gulp.series('assets'));
 });
 
