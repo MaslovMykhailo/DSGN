5ecd91eefffce0d9d2c4da3af39155aadc1a66bf
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-07-07 12:36:13 +0300

    Fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,44 @@ const del = require('del');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const gulpIf = require('gulp-if');
+const imagemin = require('gulp-imagemin');
+
+
+const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 gulp.task('less', () => {
-  return gulp.src('./frontend/less/**/*.less')
-    .pipe(sourcemaps.init())
+  return gulp.src('./frontend/less/index.less')
+    .pipe(gulpIf(isDev, sourcemaps.init()))
     .pipe(less())
-    .pipe(concat('index.css'))
-    .pipe(sourcemaps.write())
+    .pipe(gulpIf(!isDev, autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    })))
+    .pipe(gulpIf(!isDev, cleanCSS({compatibility: 'ie10'})))
+    .pipe(gulpIf(isDev, sourcemaps.write()))
     .pipe(gulp.dest('./public'));
 });
 
 gulp.task('js', () => {
   return gulp.src('./frontend/js/**/*.js')
+    .pipe(gulpIf(isDev, sourcemaps.init()))
     .pipe(concat('index.js'))
-    .pipe(uglify())
+    .pipe(gulpIf(!isDev, uglify()))
+    .pipe(gulpIf(isDev, sourcemaps.write()))
     .pipe(gulp.dest('./public'))
 });
 
-gulp.task('assets', () => {
-  return gulp.src('./frontend/assets/**/*.*', {since: gulp.lastRun('assets')})
+gulp.task('assets:html', () => {
+  return gulp.src('./frontend/assets/**/*.html', { since: gulp.lastRun('assets:html') })
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('assets:pictures', () => {
+  return gulp.src('./frontend/assets/**/*.{png,svg}', { since: gulp.lastRun('assets:pictures') })
+    .pipe(gulpIf(!isDev, imagemin()))
     .pipe(gulp.dest('./public'));
 });
 
@@ -34,13 +53,14 @@ gulp.task('clean', () => {
 });
 
 gulp.task('build', gulp.series(
-  'clean', gulp.parallel('less', 'assets', 'js'))
+  'clean', gulp.parallel('less', 'assets:html', 'assets:pictures', 'js'))
 );
 
 gulp.task('watch', () => {
   gulp.watch('./frontend/less/**/*.less', gulp.series('less'));
   gulp.watch('./frontend/js/**/*.js', gulp.series('js'));
-  gulp.watch('./frontend/assets/**/*.*', gulp.series('assets'));
+  gulp.watch('./frontend/assets/**/*.html', gulp.series('assets:html'));
+  gulp.watch('./frontend/assets/**/*.{png,svg}', gulp.series('assets:pictures'));
 });
 
 gulp.task('serve', () => {
@@ -52,3 +72,5 @@ gulp.task('serve', () => {
 });
 
 gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'serve')));
+
+gulp.task('default', gulp.series('build'));
